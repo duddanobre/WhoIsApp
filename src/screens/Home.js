f44ec655a95463cee292a5c2b171ecfc7e02c2b9
duddanobre <dudarnobre@gmail.com>
2021-05-05 15:24:52 -0300

    Header, Tela inicial com card de Agenda

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,32 +1,108 @@
-import React from 'react';
-import {View,Text } from 'react-native';
-import Icon from 'react-native-vector-icons/AntDesign';
+import React, { useState, useEffect } from 'react';
+import {View,Text, StyleSheet } from 'react-native';
+import { Card, ListItem, FAB} from 'react-native-elements';
+
+import Icon from 'react-native-vector-icons/Feather';
 
 import auth from '@react-native-firebase/auth';
 
 import Footer from '../components/footer';
 
-Icon.loadFont(); 
 
 export default function Home({navigation}) {
 
+ /* const listaAtividades = [{
+    atividade: 'Consulta geriatra ',
+    horário: '09:00 pm',
+    data: '12/05/2021'
+  },
+  {
+    atividade: 'Dentista',
+    horário: '09:00 pm',
+    data: '12/05/2021'
+  },
+  {
+  atividade: 'Terapia',
+  horário: '09:00 pm',
+  data: '12/05/2021'
+  }]; */
+
+const [agenda, setAgenda] = useState([]);
+
+ function listAgenda(){
+  firestore()
+  .collection('agenda')
+  .onSnapshot(querySnapshot => {
+      const agenda = [];
+
+      querySnapshot.forEach(documentSnapshot => {
+          agenda.push(documentSnapshot.data());
+      });
+
+      setAgenda({agenda});
+  });
+ }
+
+  useEffect(() => {
+    listAgenda();
+  }, [agenda])
 
     return (
       <View>
-        <Text>TELA INICIAL </Text>
+          <View style={styles.logoutButton}>
+            <Icon name="log-out" size={24}
+              onPress={() => auth().signOut()
+                .then(() => console.log('User signed out!'),
+                navigation.navigate("Login"))}
+            >
+            </Icon>
+            </View>
+              
+                <Card containerStyle={{padding: 0, marginTop: 50}} >
+                  <Card.Title style={{ backgroundColor: '#7200ca'}}>
+                    <View style={{left:0}}>
+                      <Text style={{color: '#fff'}}>Agenda</Text>
+                    <FAB icon={
+                      <Icon
+                        name="plus"
+                        size={24}
+                        color="white"
+                      />
+                      } 
+                      onPress={() => addAgenda()}
+                    />
+                  </View> 
+                  </Card.Title>
+                  {
+                    agenda.map((item, i) => (
+                      <ListItem key={i} bottomDivider>
+                        <ListItem.Content>
+                          <ListItem.Title style={{fontWeight: 'bold'}}>{item.atividade}</ListItem.Title>
+                          <ListItem.Title>{item.hora}</ListItem.Title>
+                          <ListItem.Title>{item.data}</ListItem.Title>
+                        </ListItem.Content>
+                        <ListItem.Chevron />
+                        <Icon name="edit" size={24} onPress={} />
+                        <Icon name="trash" size={24} />
+                      </ListItem>
+                    ))
+                  }
 
-        <Icon name="logout" size={24}
-          onPress={() => auth().signOut()
-            .then(() => console.log('User signed out!'),
-            navigation.navigate("Login"))}
-        >
-          <Text>Sair </Text>
-        </Icon>
-
+                </Card>
+               
+            
         <Footer />
-
       </View>
 
     )
   
 }
+
+const styles = StyleSheet.create({
+  logoutButton:{
+   top: 10,
+   left: 315,
+
+  },
+
+})
