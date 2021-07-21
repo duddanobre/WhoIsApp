254c3929fc77904202d0ed2664d2b7847ac0c0c9
duddanobre <dudarnobre@gmail.com>
2021-07-20 22:57:04 -0300

    Verificando se há dados disponíveis para chamar a proxima screen, passando o persistedFaceId como param

--- src/screens/Face.js
+++ src/screens/Face.js
@@ -1,21 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Text } from 'react-native';
 import {StyleSheet, ScrollView, View} from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, ListItem } from 'react-native-elements';
 import { windowHeight, windowWidth } from '../components/dimentions/Dimentions';
+import firestore, {firebase} from '@react-native-firebase/firestore';
 
 
 export default function Identificação({route, navigation}) {
+ 
+  const {paramResponse} = route.params;
 
-const {paramResponse} = route.params;
-console.log(paramResponse);
+  const [id, setId] = useState('');  
+  const [album, setAlbum] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [userId, setUserId] = useState('');
+  const [aniversario, setAniversario] = useState('');
+  const [nome, setNome] = useState('');
+  const [parentesco, setParentesco] = useState('');
+  const [persistedFaceId, setPersistedFaceId] = useState('');
+  const [visible, setVisible] = useState(false);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUserId(user.uid);
+      }
+    });
+
+  const fetchAlbum = async () => {
+    try {
+      const list = [];
+  
+      await firestore()
+        .collection('album')
+        .where('userItem', '==', userId)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            const {
+              userItem,
+              nome,
+              aniversario,
+              persistedFaceId,
+              parentesco
+            } = doc.data();
+            list.push({
+              id: doc.id,
+              userItem,
+              nome,
+              aniversario,
+              persistedFaceId,
+              parentesco
+            });
+          });
+        });
+  
+      setAlbum(list);
+       
+      if(loading){
+        setLoading(false);
+      }
+  
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  
+  useEffect(() => {
+    fetchAlbum(album);
+  }, [album]);
+
+  album.map((item) => {
+    if(paramResponse === item.persistedFaceId) 
+    
+    return  
+    <ListItem key={item.id} bottomDivider>
+      <ListItem.Content>
+        <ListItem.Title style={{fontWeight: 'bold'}}>Nome: {item.nome}</ListItem.Title>
+        <ListItem.Title>Parentesco: {item.parentesco}</ListItem.Title>
+        <ListItem.Title>Aniversário: {item.aniversario}</ListItem.Title>
+        <ListItem.Title>face: {item.persistedFaceId}</ListItem.Title>
+      </ListItem.Content>
+    </ListItem> 
+  })
+
     return (
       <ScrollView contentContainerStyle={styles.containerStyle}>
                 <Card containerStyle={{padding: 0, marginTop: 40}} >
                   <View style={{
                      backgroundColor: '#7b1fa2', height: 80}}>  
                   </View>
-                  <Text>Param: {JSON.stringify(paramResponse)}</Text>
+                 
+                  <Text>Param: {paramResponse}</Text>
                 </Card>
               
              </ScrollView>
