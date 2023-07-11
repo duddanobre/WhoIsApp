372f77dcc9bf65dd88f6f7b6952ba211afe5fd1e
Maria Eduarda Nobre <dudarnobre@gmail.com>
2023-07-11 20:37:26 -0300

    fix test

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import Login from './screens/Login';
 import Home from './screens/Home';
 import SingUp from './screens/SingUp';
@@ -14,47 +14,68 @@ import {BackHandler} from 'react-native';
 const AppDrawer = createDrawerNavigator();
 
 const Router = () => {
+  const backAction = () => {
+    return true;
+  };
 
-    const backAction = () => {
-        return true;
-      };
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', backAction);
 
-    useEffect(() => {
-        BackHandler.addEventListener("hardwareBackPress", backAction);
-    
-        return () =>
-          BackHandler.removeEventListener("hardwareBackPress", backAction);
-      }, []);
+    return () =>
+      BackHandler.removeEventListener('hardwareBackPress', backAction);
+  }, []);
 
-    return (
-        <NavigationContainer>
-            <AppDrawer.Navigator initialRouteName="Login"
-             screenOptions={{ headerShown: true, headerTintColor: "#fff", headerLeft: () => null,
-              headerStyle:{backgroundColor: "#4a0072", height: 80}
-             }}>
-               <AppDrawer.Screen name="Login" component={Login} 
-                options={{headerShown: false}}/>
-                <AppDrawer.Screen name = "Home" component={Home}
-                    options={{
-                        title: 'Who Is',
-                        headerRight: () => (
-                           <Logout />
-                        )
-                    }}
-               />
-               <AppDrawer.Screen name = "Album" component={Album}
-                 options={{swipeEnabled: false}} />
-               <AppDrawer.Screen name = "Identificação" component={Face}
-                 options={{swipeEnabled: false}} />
-                <AppDrawer.Screen name = "SingUp" component={SingUp}
-                 options={{swipeEnabled: false, headerShown: false}} />
-                <AppDrawer.Screen name = "ResetPassword" component={ResetPassword}
-                 options={{swipeEnabled: false, headerShown: false}} />
-                 <AppDrawer.Screen name = "Reconhecimento" component={Recognize}
-                 options={{swipeEnabled: false, headerShown: false}} />
-            </AppDrawer.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <AppDrawer.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: true,
+          headerTintColor: '#fff',
+          headerStyle: {backgroundColor: '#4a0072', height: 80},
+          swipeEnabled: true
+        }}>
+        <AppDrawer.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+        <AppDrawer.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Who Is',
+            headerRight: () => <Logout />,
+          }}
+        />
+        <AppDrawer.Screen
+          name="Album"
+          component={Album}
+          options={{swipeEnabled: false}}
+        />
+        <AppDrawer.Screen
+          name="Identificação"
+          component={Face}
+          options={{swipeEnabled: false}}
+        />
+        <AppDrawer.Screen
+          name="SingUp"
+          component={SingUp}
+          options={{swipeEnabled: false, headerShown: false}}
+        />
+        <AppDrawer.Screen
+          name="ResetPassword"
+          component={ResetPassword}
+          options={{swipeEnabled: false, headerShown: false}}
+        />
+        <AppDrawer.Screen
+          name="Reconhecimento"
+          component={Recognize}
+          options={{swipeEnabled: false, headerShown: false}}
+        />
+      </AppDrawer.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Router;
--- src/screens/Album.js
+++ src/screens/Album.js
@@ -1,8 +1,9 @@
-import React, { useState} from 'react';
-import { Text, TouchableOpacity, Alert } from 'react-native';
+/* eslint-disable no-undef */
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, Alert} from 'react-native';
 import {StyleSheet, ScrollView, View} from 'react-native';
-import { Card, Avatar } from 'react-native-elements';
-import { windowHeight, windowWidth } from '../components/dimentions/Dimentions';
+import {Card, Avatar} from 'react-native-elements';
+import {windowHeight, windowWidth} from '../components/dimentions/Dimentions';
 import firestore, {firebase} from '@react-native-firebase/firestore';
 import FormInput from '../components/FormInput';
 import ImagePicker from 'react-native-image-crop-picker';
@@ -20,12 +21,11 @@ const base_instance_options = {
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': key
-  }
+    'Ocp-Apim-Subscription-Key': key,
+  },
 };
 
 export default function Identificação({navigation}) {
-
   const [image, setImage] = useState(null);
   const [loading, setLoading] = useState(false);
   const [userId, setUserId] = useState('');
@@ -36,11 +36,11 @@ export default function Identificação({navigation}) {
   const [uploading, setUploading] = useState(false);
   const [transferred, setTransferred] = useState(0);
 
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUserId(user.uid);
-      }
-    });
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setUserId(user.uid);
+    }
+  });
 
   const choosePhotoFromLibrary = () => {
     ImagePicker.openPicker({
@@ -57,35 +57,38 @@ export default function Identificação({navigation}) {
     const faceId = await faceAddAPI();
     const imageUrl = await uploadImage();
     firestore()
-        .collection('album')
-        .add({
-          userItem: userId,
-          nome: nome,
-          aniversario: aniversario,
-          persistedFaceId: faceId,
-          parentesco: parentesco,
-          face: imageUrl
-        })
-        .then(() => {
-          console.log('People added', persistedFaceId, faceId);
-          setLoading(false);
-          setNome(''), setParentesco(''), setAniversario(''), setImage(null);
-          Alert.alert('Pessoa cadastrada com sucesso!')
-        })
-        .catch((error) => {
-          Alert.alert('Um erro ocorreu, verifique os dados e tente novamente!', error);
-        }); 
-  }
+      .collection('album')
+      .add({
+        userItem: userId,
+        nome: nome,
+        aniversario: aniversario,
+        persistedFaceId: faceId,
+        parentesco: parentesco,
+        face: imageUrl,
+      })
+      .then(() => {
+        console.log('People added', persistedFaceId, faceId);
+        setLoading(false);
+        setNome(''), setParentesco(''), setAniversario(''), setImage(null);
+        Alert.alert('Pessoa cadastrada com sucesso!');
+      })
+      .catch((error) => {
+        Alert.alert(
+          'Um erro ocorreu, verifique os dados e tente novamente!',
+          error,
+        );
+      });
+  };
 
   const uploadImage = async () => {
-    if( image == null ) {
+    if (image == null) {
       return null;
     }
     const uploadUri = image;
     let filename = uploadUri.substring(uploadUri.lastIndexOf('/') + 1);
 
     // Add timestamp to File Name
-    const extension = filename.split('.').pop(); 
+    const extension = filename.split('.').pop();
     const name = filename.split('.').slice(0, -1).join('.');
     filename = name + Date.now() + '.' + extension;
 
@@ -115,125 +118,130 @@ export default function Identificação({navigation}) {
       setUploading(false);
       setImage(null);
       return url;
-
     } catch (e) {
       console.log(e);
       return null;
     }
-
   };
 
-  async function faceAddAPI(){
+  async function faceAddAPI() {
     const imageUrl = await uploadImage();
     console.log('Image Url: ', imageUrl);
     try {
-      const addFaces = { ...base_instance_options };
+      const addFaces = {...base_instance_options};
       addFaces.headers['Content-Type'] = 'application/json';
       const addFaces_instance = axios.create(addFaces);
       const addFaces_res = await addFaces_instance.post(
-      `/facelists/whois-3e-facelist/persistedFaces`,
-        {url: imageUrl}
+        '/facelists/whois-3e-facelist/persistedFaces',
+        {url: imageUrl},
       );
-      console.log("face added:", addFaces_res.data.persistedFaceId);
+      console.log('face added:', addFaces_res.data.persistedFaceId);
       return addFaces_res.data.persistedFaceId;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
-    return (
-    loading && uploading ? (
-        <LottieView source={require('../assets/loading2.json')} autoPlay loop />
-      ) : (
-      <ScrollView contentContainerStyle={styles.containerStyle}>
-                <Card containerStyle={{padding: 35, marginTop: 40, marginBottom: 30}}>
-                  <View style={{
-                     backgroundColor: '#7b1fa2', height: 80}}>  
-                     <View>  
-                       <Icon
-                        style={{top: 20, left: 10}}
-                          name="arrow-left-circle"
-                          size={30}
-                          color="white"
-                          onPress={() => navigation.goBack()}
-                       />  
-                      </View>
-                  </View>
-              <View> 
-                { image == null ? (
-                  <Avatar  
-                  icon={{ name: 'image', color:'#a2a0a3', size: 60 }} 
-                  containerStyle={styles.icon} size="large" 
-                  activeOpacity={0.7}
-                  />) : (
-                  <Avatar 
-                  source={{uri: image}} containerStyle={styles.image} size="medium" 
-                  activeOpacity={0.7}
-                 />
-                )}
-                  <TouchableOpacity style={styles.buttonModal} onPress={choosePhotoFromLibrary}>
-                    <Text style={styles.buttonModalText}> Selecionar da galeria </Text>
-                  </TouchableOpacity>
-                  <FormInput
-                      labelValue={nome}
-                      onChangeText={(nomeItem) => setNome(nomeItem)}
-                      placeholderText="Nome da face"
-                      icon="scan1"
-                      keyboardType="default"
-                      autoCapitalize="none"
-                      autoCorrect={false}
-                    />
-                     <FormInput
-                      labelValue={parentesco}
-                      onChangeText={(parentItem) => setParentesco(parentItem)}
-                      placeholderText="Parentesco ex: filho"
-                      icon="scan1"
-                      keyboardType="default"
-                      autoCapitalize="none"
-                      autoCorrect={false}
-                    />
-                     <FormInput
-                      labelValue={aniversario}
-                      onChangeText={(niver) => setAniversario(niver)}
-                      placeholderText="Aniversário"
-                      icon="calendar"
-                      keyboardType="default"
-                      autoCapitalize="none"
-                      autoCorrect={false}
-                    />
-                     <FormButton
-                      buttonTitle="Cadastrar pessoa"
-                      onPress={() => {addFace()}}
-                     />
-                </View>
-              </Card>
-             </ScrollView>
-      )
-    )
-  
+  return loading && uploading ? (
+    <LottieView source={require('../assets/loading2.json')} autoPlay loop />
+  ) : (
+    <ScrollView contentContainerStyle={styles.containerStyle}>
+      <Card containerStyle={{padding: 35, marginTop: 40, marginBottom: 30}}>
+        <View
+          style={{
+            backgroundColor: '#7b1fa2',
+            height: 80,
+          }}>
+          <View>
+            <Icon
+              style={{top: 20, left: 10}}
+              name="arrow-left-circle"
+              size={30}
+              color="white"
+              onPress={() => navigation.goBack()}
+            />
+          </View>
+        </View>
+        <View>
+          {image == null ? (
+            <Avatar
+              icon={{name: 'image', color: '#a2a0a3', size: 60}}
+              containerStyle={styles.icon}
+              size="large"
+              activeOpacity={0.7}
+            />
+          ) : (
+            <Avatar
+              source={{uri: image}}
+              containerStyle={styles.image}
+              size="medium"
+              activeOpacity={0.7}
+            />
+          )}
+          <TouchableOpacity
+            style={styles.buttonModal}
+            onPress={choosePhotoFromLibrary}>
+            <Text style={styles.buttonModalText}> Selecionar da galeria </Text>
+          </TouchableOpacity>
+          <FormInput
+            labelValue={nome}
+            onChangeText={(nomeItem) => setNome(nomeItem)}
+            placeholderText="Nome da face"
+            icon="scan1"
+            keyboardType="default"
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+          <FormInput
+            labelValue={parentesco}
+            onChangeText={(parentItem) => setParentesco(parentItem)}
+            placeholderText="Parentesco ex: filho"
+            icon="scan1"
+            keyboardType="default"
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+          <FormInput
+            labelValue={aniversario}
+            onChangeText={(niver) => setAniversario(niver)}
+            placeholderText="Aniversário"
+            icon="calendar"
+            keyboardType="default"
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+          <FormButton
+            buttonTitle="Cadastrar pessoa"
+            onPress={() => {
+              addFace();
+            }}
+          />
+        </View>
+      </Card>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-  welcomeText:{
-   bottom:15,
-   fontSize: 14,
-   fontFamily: 'Quicksand-Bold',
-   color: '#fff',
-   
+  welcomeText: {
+    bottom: 15,
+    fontSize: 14,
+    fontFamily: 'Quicksand-Bold',
+    color: '#fff',
   },
-  welcome:{
+  welcome: {
     padding: 10,
     width: windowWidth,
-    height: windowHeight / 8
+    height: windowHeight / 8,
   },
-  dataText:{
+  dataText: {
     color: '#fff',
     fontFamily: 'Quicksand-Bold',
-    fontSize: 16
+    fontSize: 16,
   },
-  agendaText:{
+  agendaText: {
     fontSize: 20,
-    fontFamily: 'Quicksand-Bold'
+    fontFamily: 'Quicksand-Bold',
   },
   buttonModal: {
     backgroundColor: '#e2dee3',
@@ -244,24 +252,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderRadius: 3,
   },
-  buttonModalText:{
+  buttonModalText: {
     fontSize: 16,
     fontFamily: 'Lato-Regular',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
-image: {
+  image: {
     justifyContent: 'center',
     flex: 1,
     backgroundColor: '#e2dee3',
     width: '100%',
-    height: windowHeight/2.6,
-    padding: 15
+    height: windowHeight / 2.6,
+    padding: 15,
   },
-icon: {
-  justifyContent: 'center',
-  flex: 1,
-  backgroundColor: '#e2dee3',
-  width: '100%',
-  padding: 10
-},
+  icon: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#e2dee3',
+    width: '100%',
+    padding: 10,
+  },
 });
